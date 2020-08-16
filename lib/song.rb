962f14c4daa9807780f046b4b3a7fce1375b913c
Miguel Felix <mikeyfelix99@gmail.com>
2020-08-16 11:15:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,10 +7,15 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
+    @@artists << artist unless @@artists.include?(artist)
     @@genres << genre unless @@genres.include?(genre) 
+  
   end
   def self.count
     @@count
+  end
+  def self.artists
+    @@artists
   end
   def self.genres
   @@genres
